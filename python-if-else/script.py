20cef6d1dfaadd8ee3a6d56b851f8254a401f070
qwertycobain <qwertycobain01@gmail.com>
2022-01-09 15:36:04 -0300

    Люли

--- python-if-else/script.py
+++ python-if-else/script.py
@@ -11,8 +11,15 @@ import sys
 if __name__ == '__main__':
     n = int(raw_input().strip())
     if (n % 2) == 0:
-        print ("Not Weird")
-    
+        if (n in range(2, 5)):
+            print "Not Weird"
+            
+        elif (n in range (6, 21)):
+            print "Weird"
+
+        elif(n > 20):
+            print "Not Weird"
+
     else: 
         print ("Weird")
 
