9d9d1699e5d6b980cb980e0cefdd57e19b28c097
qwertycobain <qwertycobain01@gmail.com>
2022-01-16 14:21:48 -0300

    oh

---
+++ codes.js
@@ -0,0 +1,48 @@
+/*function greetme(name){
+    alert ("olá, "+name);
+
+}
+
+greetme("mundo");
+*/
+
+// window.console.log("ja resgata o manaus")
+
+//let a = window.document.getElementById("s")
+
+//window.console.log(a)
+
+//console.log(navigator)
+
+
+
+//let a = document.getElementsByClassName("carro")[0].innerHTML
+
+//console.log(document.getElementsByTagName("br")[0])
+
+//document.getElementById("carros").querySelectorAll(".carro"); //buscando dentro da div carros, retorna um nodelist com todos os carros 
+
+//console.log(document.getElementById("carros").querySelectorAll(".carro"))
+
+
+
+
+/*a.innerHTML = "PAM PAM, PAM PAM" 
+a.style.color = "red"
+a.style.backgroundColor = "black"
+a.style.height = "400px"
+*/
+
+//var img = document.createElement("img")
+//console.log(img)
+//img = document.getElementById("img  ")
+//img.src="a.jpg"
+
+
+//document.getElementById("carros").appendChild(img)
+
+
+//img = document.getElementById("img")
+
+
+//manipulando tag html de forma dinamica via javascript!
---
+++ main.js
@@ -0,0 +1,14 @@
+function iwilldomybest(){
+    //fs = require("fs")
+
+    //fs.open('arquivo.txt', 'w',)
+    alert("plis dont slow me down if im going to fast!")
+
+
+}
+
+
+
+//musica = ""
+
+//for (let cont = 0:musica )
